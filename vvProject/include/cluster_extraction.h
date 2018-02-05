65499014fd37f64f1c56165b1328e682ff8d4a25
Leshito <mendoza_luis-oct@sbcglobal.net>
2018-02-05 16:39:02 -0600

    add pcdFiles folder, plane seg, and clustering

---
+++ vvProject/include/cluster_extraction.h
@@ -0,0 +1,6 @@
+#ifndef CLUSTER_EXTRACTION_H
+#define CLUSTER_EXTRACTIN_H 2018
+
+int clusterExtraction(pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud,pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloudFiltered);
+
+#endif
